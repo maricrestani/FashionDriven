a6dfd8de511d804fb2bc5ee5a530c9cdd9701ac8
Mariana Crestani <marianascrestani@gmail.com>
2022-09-09 09:09:57 -0300

    versao botao ativar pedido finalizado

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
-/*const nome = prompt("Qual é o seu nome?")*/
+const nome = prompt("Qual é o seu nome?")
+
+const API = 'https://mock-api.driven.com.br/api/v4/shirts-api/shirts';
+
+function getAPI() {
+
+    const promise = axios.get(`${API}`);
+    promise.then(confirmarEncomenda);
+    //  promise.catch(erro);
+}
+
 
 let modelo, gola, tecido, imagem;
+
+const model = ["t-shirt", "long", "top-tank"];
+const neck = ["v-neck", "round", "polo"];
+const material = ["silk", "cotton", "polyester"];
+
+
+/*
+function encomendarBlusa() {
+
+    const promise = axios.post(`${API}`);
+    promise.then(confirmarEncomenda);
+    promise.catch(erro);
+
+    {
+        "model": "t-shirt" | "long" | "top-tank",
+            "neck": "v-neck" | "round" | "polo",
+                "material": "silk" | "cotton" | "polyester",
+                    "image": string no formato de url,
+                        "owner": string,
+                            "author": string
+    }
+
+}
+
+function entrarNaSala() {
+
+    novoNome = {
+        name: nome
+    }
+
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', novoNome);
+
+    promessa.then(pegarDadosChat);
+    promessa.catch(pedirNovoNome);
+}
+
+
+function erro(error) {
+    if (erro.response.status === 422) {
+        alert('****detalhar erro aqui****');
+    }
+}
+
+
+function erro(error) {
+    if (erro.response.status === 422) {
+        alert('****detalhar erro aqui****');
+    }
+}
+*/
 
 
 function selecionarModelo(modeloSelecionado) {
@@ -13,7 +73,7 @@ function selecionarModelo(modeloSelecionado) {
 
     modeloSelecionado.classList.add('borda-selecionado');
 
-    modelo = modeloSelecionado
+    modelo = modeloSelecionado;
 
     ativarBotaoFecharPedido();
 }
@@ -52,23 +112,33 @@ function selecionarTecido(tecidoSelecionado) {
 
 function salvarImagem() {
 
-    //preciso que tenha um value no Input e esse value precisa ser uma URL
-
     let image = document.querySelector('.image');
-
     imagem = image.value;
 
-    console.log(imagem);
+    ativarBotaoFecharPedido();
 }
 
+
+const button = document.querySelector('.botao')
+button.disabled = true;
 
 function ativarBotaoFecharPedido() {
 
-    if (modelo !== undefined && gola !== undefined && tecido !== undefined) {
-
-        const button = document.querySelector('.botao')
+    if (modelo !== undefined && gola !== undefined && tecido !== undefined && imagem !== undefined) {
 
         button.classList.add('pedido-confirmado')
+        button.disabled = false;
     }
 
+    console.log(nome);
+    console.log(modelo);
+    console.log(tecido);
+    console.log(gola);
+    console.log(imagem);
 }
+
+function confirmarEncomenda() {
+    alert('Sua encomenda foi confirmada!')
+}
+
+
